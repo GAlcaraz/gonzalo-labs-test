581934052ab0d1bbe8f01e1b3cf255b6e2501c93
GAlcaraz <g.alcaraz@outlook.com>
2022-07-22 14:22:39 -0300

    Add headings for SEO

--- pages/crypto.js
+++ pages/crypto.js
@@ -1,23 +1,26 @@
 import React from "react";
-import { Flex, Text, useToast } from "@chakra-ui/react";
-import { useRouter } from "next/router";
+import { Flex } from "@chakra-ui/react";
 import MetamaskDashboard from "components/MetamaskDashboard";
+import { NextSeo } from "next-seo";
 
 export default function Layout({ ...props }) {
-  const router = useRouter();
-  const toast = useToast();
-
   return (
-    <Flex
-      flex={1}
-      direction="column"
-      align="center"
-      justify="center"
-      m="0 auto"
-      px={6}
-      {...props}
-    >
-      <MetamaskDashboard />
-    </Flex>
+    <>
+      <NextSeo
+        title="Gonzalo's Labs - Crypto"
+        description="Metamask connection showcase."
+      />
+      <Flex
+        flex={1}
+        direction="column"
+        align="center"
+        justify="center"
+        m="0 auto"
+        px={6}
+        {...props}
+      >
+        <MetamaskDashboard />
+      </Flex>
+    </>
   );
 }
--- pages/test/[[...number]].js
+++ pages/test/[[...number]].js
@@ -5,6 +5,7 @@ import { Formik } from "formik";
 import * as Yup from "yup";
 import TextInput from "components/forms/TextInput";
 import { integerToWords } from "pages/api/convert";
+import { NextSeo } from "next-seo";
 
 const schema = Yup.object().shape({
   number: Yup.number()
@@ -29,58 +30,66 @@ export default function Layout({ number, words, ...props }) {
   }, [number, router.query.number]);
 
   return (
-    <Flex
-      flex={1}
-      direction="column"
-      align="center"
-      justify="center"
-      m="0 auto"
-      px={6}
-      {...props}
-    >
-      <Text
-        textAlign="center"
-        fontSize={{ base: "2xl", sm: "4xl" }}
-        color="black"
-        mb={12}
+    <>
+      <NextSeo
+        title="Gonzalo's Labs - Numbers Test"
+        description="API Route showcase."
+      />
+      <Flex
+        flex={1}
+        direction="column"
+        align="center"
+        justify="center"
+        m="0 auto"
+        px={6}
+        {...props}
       >
-        {words}
-      </Text>
-      <Formik
-        initialValues={{
-          number: number || "",
-        }}
-        validationSchema={schema}
-        onSubmit={(values) => {
-          router.push(`/test/${values.number}`, undefined, { shallow: false });
-        }}
-        // enableReinitialize={true}
-      >
-        {({ handleSubmit }) => {
-          return (
-            <form onSubmit={handleSubmit}>
-              <Flex direction="column">
-                <TextInput
-                  name="number"
-                  label="Enter your positive integer"
-                  type="number"
-                  placeholder=""
-                  mb={3}
-                />
-                <Button
-                  size="lg"
-                  colorScheme="orange"
-                  borderRadius="md"
-                  type="submit"
-                >
-                  Convert
-                </Button>
-              </Flex>
-            </form>
-          );
-        }}
-      </Formik>
-    </Flex>
+        <Text
+          textAlign="center"
+          fontSize={{ base: "2xl", sm: "4xl" }}
+          color="black"
+          mb={12}
+        >
+          {words}
+        </Text>
+        <Formik
+          initialValues={{
+            number: number || "",
+          }}
+          validationSchema={schema}
+          onSubmit={(values) => {
+            router.push(`/test/${values.number}`, undefined, {
+              shallow: false,
+            });
+          }}
+          // enableReinitialize={true}
+        >
+          {({ handleSubmit }) => {
+            return (
+              <form onSubmit={handleSubmit}>
+                <Flex direction="column">
+                  <TextInput
+                    name="number"
+                    label="Enter your positive integer"
+                    type="number"
+                    placeholder=""
+                    mb={3}
+                  />
+                  <Button
+                    size="lg"
+                    colorScheme="orange"
+                    borderRadius="md"
+                    type="submit"
+                  >
+                    Convert
+                  </Button>
+                </Flex>
+              </form>
+            );
+          }}
+        </Formik>
+      </Flex>
+    </>
   );
 }
 
